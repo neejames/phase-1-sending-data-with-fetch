5a00604b65082507cf850a5574048d09a5dd4df2
Neechelle James <neechelle.james@gmail.com>
2023-05-22 10:15:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        }),
+    };
+    return fetch(`http://localhost:3000/users`, configurationObj)
+    .then(resp => resp.json())
+    .then(newUser => document.body.innerHTML = newUser['id'])
+    .catch(error => document.body.innerHTML = error.message)
+}
+
